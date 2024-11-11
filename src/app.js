5ad868a5615a7f72d455f21b3e34428f6d5e203d
thanhthanhm061 <thanhthanhm061@gmail.com>
2024-11-11 10:01:35 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import productRouter from './routes/product.js';  // Thêm phần mở rộng .js
-import authRouter from './routes/auth.js';       // Thêm phần mở rộng .js
+import productRouter from './routes/product.js';
+import authRouter from './routes/auth.js';       
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
@@ -14,17 +14,25 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // connect db
-connectDB(process.env.DB_URI);
+// connectDB(process.env.DB_URI);
+connectDB(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 20000, // Thời gian timeout 20 giây
+  })
+  .then(() => console.log("Kết nối thành công tới MongoDB"))
+  .catch((err) => console.error("Lỗi kết nối MongoDB:", err));
 
 // routes
 app.use('/api', productRouter);
 app.use('/api', authRouter);
 
 
+export const viteNodeApp = app;
 
+// Khởi động server
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-export const viteNodeApp = app;
+
